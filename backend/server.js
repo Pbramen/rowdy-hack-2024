2f9032e81598af53c99734aa93d1b38b01658cd6
Peter Banh <peterbanhe.e@gmail.com>
2024-02-24 16:32:07 -0600

    mongoose connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
+const mongoose = requrie("mongoose");
 
-app.listen(4000, () => {
-    console.log("listening to port 4000...")
-})
+console.log(process.env.URL);
+
+mongoose.connect(process.env.URL)
+    .then(() => {
+        app.listen(80, () => {
+            console.log("listening to port 4000...")
+        })
+    })
+    .catch((error) => { 
+        //uh oh
+        console.log("MongoDB connection failed", error)
+    })
 
 app.get("/", (req, res) => { 
-    res.status(200).json({'status':'test'});
+    res.status(400).render("<h1>You shouldn't be here >:( </h1>");
 })
